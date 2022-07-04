84df610f2898cd73fe27038c86bf48da39a73342
MustafaAkipek <101327731+MustafaAkipek@users.noreply.github.com>
2022-07-04 11:54:14 +0300

    Add files via upload

---
+++ akrepyelkovanaçı.py
@@ -0,0 +1,18 @@
+print("Akrep ile Yelkovan Arasındaki Açıyı Bulan Program")
+zaman = input("Lutfen Saati Giriniz: ")
+time = zaman.split(":")
+a = time[0]
+b = time[1]
+print("Saat:",a)
+print("Dakika:",b)
+ampm = int(a)%12
+akrep = ampm*30
+yelkovan = int(b)*6
+ilerlemepayi = int(b)/2
+açı = akrep-yelkovan
+sonuç = abs(açı)
+if akrep > yelkovan:
+    sonuç += ilerlemepayi
+elif yelkovan > akrep:
+    sonuç -= ilerlemepayi
+print("Akrep ve Yelkovan arasındaki açı: ",sonuç)
